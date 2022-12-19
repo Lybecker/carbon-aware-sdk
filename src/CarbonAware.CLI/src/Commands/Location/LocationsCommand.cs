ff3edf76beb855cce73eb1eed235cf4978e6e8f9
Priti <pritipathak@microsoft.com>
2022-12-19 15:13:30 -0500

    Added CLI Command for getting all locations

---
+++ src/CarbonAware.CLI/src/Commands/Location/LocationsCommand.cs
@@ -0,0 +1,28 @@
+﻿using CarbonAware.Aggregators.CarbonAware;
+using CarbonAware.Aggregators.Emissions;
+using CarbonAware.CLI.Commands.Emissions;
+using CarbonAware.Interfaces;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Text.Json;
+
+namespace CarbonAware.CLI.Commands.Location;
+
+public class LocationsCommand : Command
+{
+    public LocationsCommand() : base("locations", LocalizableStrings.LocationsCommandDescription)
+    {
+        this.SetHandler(this.Run);
+    }
+
+    internal void Run(InvocationContext context)
+    {
+        var serviceProvider = context.BindingContext.GetService(typeof(IServiceProvider)) as IServiceProvider ?? throw new NullReferenceException("ServiceProvider not found");
+        var locationSource = serviceProvider.GetService(typeof(ILocationSource)) as ILocationSource ?? throw new NullReferenceException("ILocationSource not found");
+
+        var locations = locationSource.GetAllGeopositionLocationsAsync();
+        var serializedOuput = JsonSerializer.Serialize(locations);
+        context.Console.WriteLine(serializedOuput);
+        context.ExitCode = 0;
+    }
+}
